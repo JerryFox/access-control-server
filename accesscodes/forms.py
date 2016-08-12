4241674f2116e16749d6d9ee81dbfd17b87e7a47
Jaroslav Vysoky <jaroslav.vysoky@gmail.com>
2016-08-12 13:02:06 +0200

    user_name hidden, text changes - Mr.Petrik demands

--- accesscodes/forms.py
+++ accesscodes/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.forms.widgets import HiddenInput
+
 
 class GetCodeForm(forms.Form):
     user_email = forms.EmailField(label=mark_safe('<strong>e-mail:</strong>'), max_length=100)
     user_name = forms.CharField(label='uživatelské jméno:', max_length=100, 
-        required=False)
+        required=False, widget = forms.HiddenInput())
     firstname = forms.CharField(label='křestní jméno:', max_length=100, 
         required=False)
     surname = forms.CharField(label='příjmení:', max_length=100, 
